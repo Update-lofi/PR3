c817a97c1b398c026458ee4aaae5968db340f620
Update-lofi <205054339+Update-lofi@users.noreply.github.com>
2026-04-25 23:20:30 +0500

    Добавляем маршрут удаления в app.py

--- todo_app_Task2/app.py
+++ todo_app_Task2/app.py
@@ -30,5 +30,13 @@ def add_task():
         save_tasks(tasks)
     return redirect('/')
 
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    if 0 <= task_id < len(tasks):
+        tasks.pop(task_id)
+        save_tasks(tasks)
+    return redirect('/')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
