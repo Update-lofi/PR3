16b88a8d8e9b1425bc5a6d76694576d54cbba77e
Update-lofi <205054339+Update-lofi@users.noreply.github.com>
2026-04-25 23:17:25 +0500

    Обновление кода app.py

--- todo_app_Task2/app.py
+++ todo_app_Task2/app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-
 tasks = ['Купить хлеб', 'Сделать уроки', 'Позвонить другу']
 
 @app.route('/')
 def index():
     return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    new_task = request.form['task']
+    if new_task:
+        tasks.append(new_task)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
